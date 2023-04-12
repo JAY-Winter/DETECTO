382507ecb95e36c23971e2c83d1982f17153fc70
yonghoon <fishlittle2003@gmail.com>
2023-04-12 09:15:36 +0900

    [SETTING] 클리닝

--- cosmos/src/App.tsx
+++ cosmos/src/App.tsx
@@ -1,21 +1,8 @@
-import React, { useEffect } from 'react'
-import { RecoilRoot } from 'recoil'
-import CharacterCounter from './CharacterCounter'
-import axios from 'axios'
+import React from 'react'
 
 function App() {
-  const lintTest = "린트 테스트를 위한 용도";
-  const fetchData = async () => {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-    console.log(response.data);
-  }
-  useEffect(() => {
-    fetchData();
-  }, [])
   return (
-    <RecoilRoot>
-      <CharacterCounter />
-    </RecoilRoot>
+    <div>App</div>
   )
 }
 
--- cosmos/src/main.tsx
+++ cosmos/src/main.tsx
@@ -1,18 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/tico",
-    element: <App />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <RouterProvider router={router} />
+  <App />
 )
