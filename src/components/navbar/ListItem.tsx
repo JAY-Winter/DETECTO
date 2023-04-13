2b311b502905487b18842610d582192cd5f05b7c
yonghoon <fishlittle2003@gmail.com>
2023-04-13 17:37:37 +0900

    [FEAT] 라우팅 설정 및 네비게이션 바 UI 선언적으로 구현

---
+++ src/components/navbar/ListItem.tsx
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled'
+import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+
+type ListItemProps = {
+  icon: React.ReactNode,
+  label: string,
+  pathName: string,
+  currentPathName: string,
+  clickHandler: (e: React.MouseEvent<HTMLLIElement>) => void
+}
+
+function ListItem({icon, label, pathName, currentPathName, clickHandler}: ListItemProps) {
+  const navigate = useNavigate();
+  const handleClickItem = (e: React.MouseEvent<HTMLLIElement>) => {
+    clickHandler(e);
+    navigate(pathName);
+  }
+  return (
+    <StyledLi currentPathName={currentPathName} pathName={pathName} onClick={(e) => handleClickItem(e)}>
+      {icon}
+      <p>{label}</p>
+    </StyledLi>
+  )
+}
+
+const StyledLi = styled.li<{currentPathName: string, pathName: string}>`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  color: ${props => props.currentPathName === props.pathName ? props.theme.palette.primary.main : props.theme.palette.text.secondary};
+  font-weight: ${props => props.currentPathName === props.pathName ? "bold" : "normal"};
+  cursor: pointer;
+  margin-bottom: 30px;
+  &:hover {
+    color: ${props => props.theme.palette.primary.main}
+  }
+  p {
+    font-size: 1.3rem;
+    margin-left: 10px;
+  }
+`
+
+export default ListItem
