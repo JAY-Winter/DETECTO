8bbd876dd79047f0b003397402b1601ad68c586b
sangjun bae <qotkdwns1324@naver.com>
2023-04-14 17:16:24 +0900

    [FEAT] 대시보드, 카드 모바일 UI 작성

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,9 +1,8 @@
 import DashboardDatePicker from '@components/dashboard/DatePicker';
 import DashboardEquipmentFilter from '@components/dashboard/EquipmentFilter';
 import DashboardSafetyIssue from '@components/dashboard/SafetyIssue';
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import React from 'react';
+
 
 function DashboardPage() {
   return (
@@ -19,11 +18,10 @@ function DashboardPage() {
 const DashboardDiv = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
 
   width: 100%;
   
-  align-items: center;
-
   margin: 1rem;
 `
 
