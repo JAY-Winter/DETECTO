73833b95bea65acb99bbc4b20f9ee50f03a12881
yonghoon <fishlittle2003@gmail.com>
2023-04-18 14:39:23 +0900

    [FEAT] 보호구 관리 페이지 드롭다운 메뉴 UI 추가 및 grid 레이아웃 설정

--- src/pages/EquipmentManagePage.tsx
+++ src/pages/EquipmentManagePage.tsx
@@ -1,6 +1,6 @@
 import EquipmentCard from '@components/equipmentManage/EquipmentCard';
 import styled from '@emotion/styled';
-import { Button } from '@mui/material';
+import { Button, css } from '@mui/material';
 import { AddCircleOutline } from '@mui/icons-material';
 import { EquipmentType } from 'EquipmentTypes';
 import { useRecoilState } from 'recoil';
@@ -12,9 +12,12 @@ function EquipmentManagePage() {
 
   // 장비 삭제 핸들러: 카드로부터 ID를 입력받아 삭제한다
   const deleteHandler = (willDeleteID: number) => {
-    setEquipment((oldState) => {
-      return oldState.filter(item => item.id !== willDeleteID);
-    })
+    const decision = confirm("삭제하시겠습니까??");
+    if (decision) {
+      setEquipment((oldState) => {
+        return oldState.filter(item => item.id !== willDeleteID);
+      })
+    }
   }
 
   // 장비 활성화 여부 토글링 핸들러: 카드로부터 ID를 입력받아 토글링한다
@@ -36,7 +39,7 @@ function EquipmentManagePage() {
     const randomID = getRandomNumber(1, 10000000);
     const randomName = getRandomString(5);
     const randomDesc = getRandomString(10);
-    const randomImgURL = `https://unsplash.it/150/200?image=${getRandomNumber(1, 1000)}`;
+    const randomImgURL = `https://unsplash.it/150/200?image=${getRandomNumber(1, 100)}`;
     const randomIsActive = getRandomBool();
 
     const newItem: EquipmentType = {
@@ -54,37 +57,44 @@ function EquipmentManagePage() {
   }
 
   return (
-    <EquipmentDiv>
+    <EquipmentManageDiv>
       <h1>보호구 관리</h1>
       <EquipmentCardDiv>
-        {equipments.map(equipment => <EquipmentCard key={equipment.id} equipment={equipment} onDelete={deleteHandler} onToggleAccessibility={toggleHandler} />)}
+        {equipments.map(equipment => <EquipmentCard key={equipment.id} equipment={equipment} onDelete={deleteHandler} onToggleActiveness={toggleHandler} />)}
+        <button css={tempStyle} onClick={addItem}>+</button>
       </EquipmentCardDiv>
-      <EquipmentAddButton variant="contained" onClick={addItem}>
-        <AddCircleOutline />
-      </EquipmentAddButton>
-      <hr />
-    </EquipmentDiv>
+    </EquipmentManageDiv>
   );
 }
 
 export default EquipmentManagePage;
 
-const EquipmentDiv = styled.div`
+const tempStyle = css`
+  width: 22rem;
+  height: 100%;
+  min-height: 350px;
+  border: none;
+  cursor: pointer;
+`
+
+const EquipmentManageDiv = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
-  width: 100%;
-  height: 100%;
   align-items: center;
-  margin: 1rem;
+  /* background-color: red; */
+  margin: 3rem 0px;
+  padding: 0px 1rem;
 `;
 
 const EquipmentCardDiv = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-
+  display: grid;
+  place-items: center;
+  grid-template-columns: repeat(auto-fill, minmax(22rem, 1fr));
+  column-gap: 30px;
+  row-gap: 30px;
   width: 100%;
+  margin-top: 1.5rem;
 `;
 
 const EquipmentAddButton = styled(Button)`
