bde0ab75dbdf2165254342f7357cc74f9f2dc5d8
윤소현 <wja06018@naver.com>
2023-04-27 09:41:25 +0900

    [FEAT] RabbitMQ 테스트

---
+++ Flask/rabbitMQ/send.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import pika
+
+__url = 'k8d201.p.ssafy.io'
+__port = 15672
+__vhost = '/'
+__cred = pika.PlainCredentials('guest', 'guest')
+__queue = 'hello'; 
+
+# RabbitMQ 서버에 연결
+connection = pika.BlockingConnection(pika.ConnectionParameters(__url, __port, __vhost, __cred))
+channel = connection.channel()
+
+# 큐 생성
+channel.queue_declare(queue=__queue)
+
+# 절대 메시지 큐로 바로 전송하지 않음. exchange를 거쳐서 전송됨.
+# default exchange는 빈 문자열
+# routing_key는 큐 이름 적어주면 됨.
+channel.basic_publish(exchange='',
+                      routing_key=__queue,
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+
+# 연결 종료
+connection.close()
