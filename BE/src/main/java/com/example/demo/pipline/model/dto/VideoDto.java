b05e4076db18e61191e39d3f71c0c237b2a6c15e
정재현 <rubat0@naver.com>
2023-04-28 10:02:45 +0900

    [FEATURE] 소켓 통신을 이용한 감지 사진 렌더링

---
+++ BE/src/main/java/com/example/demo/pipline/model/dto/VideoDto.java
@@ -0,0 +1,10 @@
+package com.example.demo.pipline.model.dto;
+
+import java.util.List;
+
+public class VideoDto {
+    private Boolean flag;
+    public String frame;
+    private List<Integer> detected_classes;
+}
+
