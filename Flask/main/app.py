2b7e2f077804cd3e3b4daa3f5cbf5faa5f2a2563
성광현 <gch03944@gmail.com>
2023-04-30 19:44:52 +0900

    [FEAT] object storage 연결 및 테스트

--- Flask/main/app.py
+++ Flask/main/app.py
@@ -2,31 +2,26 @@ from flask import Flask, render_template, request
 from ultralytics import YOLO
 from .stream.receive import upload_image
 from .stream.send import video_feed
-from main.tools.cloud import cloud
+
 
 def create_app():
-  app = Flask(__name__)
-  cctv_list = {}
-  model = YOLO('model/best.pt')
-  a = cloud()
-  response = a.client.list_buckets()
-    
-  for bucket in response.get('Buckets', []):
-      print(bucket.get('Name'))
-      
-  @app.route('/', defaults={'cctv_id': '0'})
-  @app.route('/<cctv_id>')
-  def index(cctv_id):
-    return render_template('index.html', cctv_id=cctv_id)
+    app = Flask(__name__)
+    cctv_list = {}
+    model = YOLO("model/best.pt")
 
-  # CCTV로부터 영상(이미지)를 받아오기
-  @app.route('/upload', methods=['POST'])
-  def upload():
-    return upload_image(request, model, cctv_list)
+    @app.route("/", defaults={"cctv_id": "0"})
+    @app.route("/<cctv_id>")
+    def index(cctv_id):
+        return render_template("index.html", cctv_id=cctv_id)
 
-  # html로 detecting된 영상 전송
-  @app.route('/stream/<int:cctv_id>')
-  def stream(cctv_id):  
-    return video_feed(cctv_id, cctv_list)
+    # CCTV로부터 영상(이미지)를 받아오기
+    @app.route("/upload", methods=["POST"])
+    def upload():
+        return upload_image(request, model, cctv_list)
 
-  return app
+    # html로 detecting된 영상 전송
+    @app.route("/stream/<int:cctv_id>")
+    def stream(cctv_id):
+        return video_feed(cctv_id, cctv_list)
+
+    return app
---
+++ Flask/main/repasitory/repasitory.py
@@ -0,0 +1,10 @@
+from main.tools.database import db
+from sqlalchemy import ForeignKey
+
+
+class Report(db.Model):
+    __tablename__ = "reports"
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, ForeignKey("users.id"), nullable=False)
+    time = db.Column(db.BigInteger, nullable=False)
+    url = db.Column(db.String(64), nullable=False)
