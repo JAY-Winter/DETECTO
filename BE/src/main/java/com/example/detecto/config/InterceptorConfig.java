2ac25c20b60e0ad3a64aed744ba6c4c367db50a3
이석원 <tjrdnjs67@naver.com>
2023-05-03 09:10:02 +0900

    🌚[be:feat] login session and cookie 처리

--- BE/src/main/java/com/example/detecto/config/InterceptorConfig.java
+++ BE/src/main/java/com/example/detecto/config/InterceptorConfig.java
@@ -1,6 +1,7 @@
 package com.example.detecto.config;
 
 
+import com.example.detecto.interceptor.SessionInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,13 +11,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class InterceptorConfig implements WebMvcConfigurer {
 
-//    private final SessionInterceptor sessionInterceptor;
-//
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(sessionInterceptor)
-//                .addPathPatterns("/**") // 세션 인증을 확인할 URL 패턴을 설정합니다.
-//                .excludePathPatterns("/user/login"); // 인증을 확인하지 않을 URL 패턴을 설정합니다.
-//    }
+    private final SessionInterceptor sessionInterceptor;
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(sessionInterceptor)
+                .addPathPatterns("/**") // 세션 인증을 확인할 URL 패턴을 설정합니다.
+                .excludePathPatterns("/user/login"); // 인증을 확인하지 않을 URL 패턴을 설정합니다.
+    }
 }
