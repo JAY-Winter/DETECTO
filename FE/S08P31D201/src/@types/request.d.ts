cadd71521ac6e4d6b6159d57e512e3d6f8635ce6
yonghoon <fishlittle2003@gmail.com>
2023-05-03 17:55:56 +0900

    [REFACT] useAxios 구현

--- FE/S08P31D201/src/@types/request.d.ts
+++ FE/S08P31D201/src/@types/request.d.ts
@@ -1,16 +1,13 @@
-
-
 // User 타입 선언
 declare module "AxiosRequest" {
 	export type useAxiosParmas = {
-    isFire: boolean,
     requestObj: {
       url: string,
       method: 'get' | 'post' | 'put' | 'delete' | 'patch',
       body?: object
     },
-    tryHandler: () => void,
-    catchHandler: () => void,
-    finallyHandler: () => void
+    tryHandler?: () => void,
+    catchHandler?: () => void,
+    finallyHandler?: () => void
   }
 }
--- FE/S08P31D201/src/hooks/useAxios.ts
+++ FE/S08P31D201/src/hooks/useAxios.ts
@@ -2,9 +2,22 @@ import { useAxiosParmas } from "AxiosRequest";
 import axios from "axios"
 import { useEffect, useState } from "react"
 
+/* 사용 예시
+  const reqeustObj = {
+    url: "posts/1",
+    method: 'get' as const,
+    body: undefined
+  }
+  
+  const [data, isLoading, status, setIsFire] = useAxios({requestObj: reqeustObj});
+
+  <button onClick={() => setIsFire(true)}>요청 보내기</button>
+*/
+
 // isFire, 요청객체(메소드, 세부 요청 url, 실어보낼 데이터), 요청 메소드, 에러 핸들러, finally 메소드를 입력 받음
-function useAxios({ isFire, requestObj, tryHandler, catchHandler, finallyHandler }: useAxiosParmas) {
+function useAxios({ requestObj, tryHandler, catchHandler, finallyHandler }: useAxiosParmas) {
   // 데이터, 로딩 상태, 데이터, 응답 코드
+  const [isFire, setIsFire] = useState(false);
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [status, setStatus] = useState<number>();
@@ -21,10 +34,19 @@ function useAxios({ isFire, requestObj, tryHandler, catchHandler, finallyHandler
         withCredentials: true
       })
       console.log(response.data);
+      if (tryHandler) {
+        tryHandler();
+      }
     } catch {
+      if (catchHandler) {
+        catchHandler();
+      }
 
     } finally {
-
+      setIsFire(false);
+      if (finallyHandler) {
+        finallyHandler();
+      }
     }
   }
 
@@ -34,7 +56,7 @@ function useAxios({ isFire, requestObj, tryHandler, catchHandler, finallyHandler
     }
   }, [isFire])
 
-  return [data, isLoading, status]
+  return [data, isLoading, status, setIsFire] as const;
 }
 
 export default useAxios
