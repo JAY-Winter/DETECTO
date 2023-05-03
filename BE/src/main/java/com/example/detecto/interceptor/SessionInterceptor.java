577229489ae6bd0450714d94195fdb01fc4c54b4
이석원 <tjrdnjs67@naver.com>
2023-05-03 13:12:55 +0900

    🌚[be:test] cicd test

--- BE/src/main/java/com/example/detecto/interceptor/SessionInterceptor.java
+++ BE/src/main/java/com/example/detecto/interceptor/SessionInterceptor.java
@@ -1,47 +1,47 @@
-package com.example.detecto.interceptor;
-
-import com.example.detecto.dto.UserDto;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-
-import java.io.IOException;
-
-@Component
-public class SessionInterceptor implements HandlerInterceptor {
-
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-        HttpSession session = request.getSession(false);
-
-        if (session == null) {
-            return extracted(response);
-        } else {
-            Object userObj = session.getAttribute("user");
-            if (userObj instanceof UserDto) {
-                // 인증 성공
-                return true;
-            } else {
-                return extracted(response);
-            }
-        }
-    }
-
-    private boolean extracted(HttpServletResponse response) throws IOException {
-        // 쿠키 삭제
-        Cookie sessionCookie = new Cookie("JSESSIONID", "");
-        sessionCookie.setMaxAge(0);
-        sessionCookie.setHttpOnly(true);
-        sessionCookie.setPath("/");
-        response.addCookie(sessionCookie);
-
-        // 인증 실패 응답
-        response.sendError(HttpStatus.BAD_REQUEST.value(), "fail Authentication");
-        return false;
-    }
-}
+//package com.example.detecto.interceptor;
+//
+//import com.example.detecto.dto.UserDto;
+//import jakarta.servlet.http.Cookie;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.servlet.http.HttpSession;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//
+//import java.io.IOException;
+//
+//@Component
+//public class SessionInterceptor implements HandlerInterceptor {
+//
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//
+//        HttpSession session = request.getSession(false);
+//
+//        if (session == null) {
+//            return extracted(response);
+//        } else {
+//            Object userObj = session.getAttribute("user");
+//            if (userObj instanceof UserDto) {
+//                // 인증 성공
+//                return true;
+//            } else {
+//                return extracted(response);
+//            }
+//        }
+//    }
+//
+//    private boolean extracted(HttpServletResponse response) throws IOException {
+//        // 쿠키 삭제
+//        Cookie sessionCookie = new Cookie("JSESSIONID", "");
+//        sessionCookie.setMaxAge(0);
+//        sessionCookie.setHttpOnly(true);
+//        sessionCookie.setPath("/");
+//        response.addCookie(sessionCookie);
+//
+//        // 인증 실패 응답
+//        response.sendError(HttpStatus.BAD_REQUEST.value(), "fail Authentication");
+//        return false;
+//    }
+//}
