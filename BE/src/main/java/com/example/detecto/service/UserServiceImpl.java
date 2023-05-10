ab475530e74e3514d6235e2d00b18757eb71b393
이석원 <tjrdnjs67@naver.com>
2023-05-10 12:53:23 +0900

    🌚[be:fix] code Refactoring 3

--- BE/src/main/java/com/example/detecto/service/UserServiceImpl.java
+++ BE/src/main/java/com/example/detecto/service/UserServiceImpl.java
@@ -2,11 +2,14 @@ package com.example.detecto.service;
 
 import com.example.detecto.dto.UserDto;
 import com.example.detecto.entity.User;
+import com.example.detecto.exception.DatabaseFetchException;
 import com.example.detecto.exception.DoesNotExistData;
 import com.example.detecto.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 
 @Slf4j
@@ -18,7 +21,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User login(UserDto userDto) {
-        return userRepository.findByIdAndPassword(userDto.getId(), userDto.getPassword()).orElseThrow(() -> new DoesNotExistData("아이디가 존재하지 않습니다."));
+        return getUser(userDto);
     }
 
     @Override
@@ -28,8 +31,14 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public void deleteFcmToken(UserDto userDto) {
-        User user = userRepository.findByIdAndPassword(userDto.getId(), userDto.getPassword()).orElseThrow(() -> new DoesNotExistData("아이디가 존재하지 않습니다."));
+        User user = getUser(userDto);
+
         user.setFcmToken(null);
         userRepository.save(user);
+    }
+
+    private User getUser(UserDto userDto) {
+        userRepository.findById(userDto.getId()).orElseThrow(() -> new DoesNotExistData("아이디가 존재하지 않습니다."));
+        return userRepository.findByIdAndPassword(userDto.getId(), userDto.getPassword()).orElseThrow(() -> new DoesNotExistData("비밀번호가 틀렸습니다."));
     }
 }
