76a995c9ebb00e304e451e6bf3a7e0f363a1a74f
이석원 <tjrdnjs67@naver.com>
2023-05-12 16:21:52 +0900

    🌚[be:fix] code Refactoring 14

--- BE/src/main/java/com/example/detecto/config/WebConfig.java
+++ BE/src/main/java/com/example/detecto/config/WebConfig.java
@@ -1,11 +1,24 @@
 package com.example.detecto.config;
 
+import com.example.detecto.interceptor.SessionInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final SessionInterceptor sessionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(sessionInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/api/user/login");
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
