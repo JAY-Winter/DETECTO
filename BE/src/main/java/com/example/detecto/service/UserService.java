787e25db5dd205d56785ecdef4ea2deead7a25b8
이석원 <tjrdnjs67@naver.com>
2023-05-12 16:53:48 +0900

    🌚[be:fix] code Refactoring 16

--- BE/src/main/java/com/example/detecto/service/UserService.java
+++ BE/src/main/java/com/example/detecto/service/UserService.java
@@ -3,8 +3,6 @@ package com.example.detecto.service;
 import com.example.detecto.dto.UserDto;
 import com.example.detecto.entity.User;
 
-import java.util.Optional;
-
 public interface UserService{
     User login(UserDto userDto);
 
