a573fefb9323b70c015c318d6ff5ef5a78bd2953
yonghoon <fishlittle2003@gmail.com>
2023-05-15 14:15:06 +0900

    [FEAT] FCM 동작 코드

--- FE/S08P31D201/public/firebase-messaging-sw.js
+++ FE/S08P31D201/public/firebase-messaging-sw.js
@@ -1,43 +1,10 @@
-// importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-// importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyApxj3R7G6FBh_iA6J7iVklXAdPx68VdqQ",
-//   authDomain: "detecto-92de8.firebaseapp.com",
-//   projectId: "detecto-92de8",
-//   storageBucket: "detecto-92de8.appspot.com",
-//   messagingSenderId: "849569437999",
-//   appId: "1:849569437999:web:f0896484f6e1c67ae3e9da",
-//   measurementId: "G-SPCW1TNPV7"
-// };
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDPYXra_6vq8OhKYtJVJhY75KW8G5nQWB4",
-//   authDomain: "detecto-49eca.firebaseapp.com",
-//   projectId: "detecto-49eca",
-//   storageBucket: "detecto-49eca.appspot.com",
-//   messagingSenderId: "214750770681",
-//   appId: "1:214750770681:web:d847bbd4d7df57ffb3f928"
-// };
-
-// firebase.initializeApp(firebaseConfig);
-// const messaging = firebase.messaging();
-
-// messaging.onBackgroundMessage((payload) => {
-//   const notificationTitle = payload.notification.title;
-//   const notificationOptions = {
-//     body: payload.notification.body,
-//     icon: payload.notification.image,
-//   };
-
-//   self.registration.showNotification(notificationTitle, notificationOptions);
-// });
-
 const pushHandler = async (e) => {
-  console.log("push 알림이 오긴 온다:", e);
   const data = e.data.json();
-  const {title, body} = data.notification;
-  console.log(title, body);
+  const { title, body } = data.notification;
+  const notificationOptions = {
+    body: body,
+  };
+  self.registration.showNotification(title, notificationOptions);
 };
 
 self.addEventListener('push', pushHandler);
