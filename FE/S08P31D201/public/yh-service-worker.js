8291e0517707edde1f0ee9e80800990092bd9c73
YongHoon <fishlittle2003@gmail.com>
2023-05-16 14:54:15 +0900

    [TEST] iOS 모바일 수신 가능여부 테스트

--- FE/S08P31D201/public/yh-service-worker.js
+++ FE/S08P31D201/public/yh-service-worker.js
@@ -1,24 +1,27 @@
 const pushHandler = (event) => {
+  // const { title, body } = event.data.json();
+  // const options = {
+  //   body: body,
+  //   icon: '/icons/icon-512x512.png',
+  //   vibrate: [100, 50, 100],
+  //   data: {
+  //     dateOfArrival: Date.now()
+  //   },
+  //   actions: [
+  //     {
+  //       action: 'confirm',
+  //       title: '확인'
+  //     },
+  //     {
+  //       action: 'close',
+  //       title: '닫기'
+  //     },
+  //   ],
+  // };
+  // event.waitUntil(self.registration.showNotification(title, options));
+
   const { title, body } = event.data.json();
-  const options = {
-    body: body,
-    icon: '/icons/icon-512x512.png',
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now()
-    },
-    actions: [
-      {
-        action: 'confirm',
-        title: '확인'
-      },
-      {
-        action: 'close',
-        title: '닫기'
-      },
-    ],
-  };
-  event.waitUntil(self.registration.showNotification(title, options));
+  event.waitUntil(self.registration.showNotification(title, {body}));
 };
 
 self.addEventListener('push', pushHandler);
