66fa62cc2c3c34caca2ac953ded8794b6a3ae5c7
YongHoon <fishlittle2003@gmail.com>
2023-05-16 15:11:31 +0900

    [TEST] 서비스워커 수정 취소

--- FE/S08P31D201/public/yh-service-worker.js
+++ FE/S08P31D201/public/yh-service-worker.js
@@ -1,27 +1,24 @@
 const pushHandler = (event) => {
-  // const { title, body } = event.data.json();
-  // const options = {
-  //   body: body,
-  //   icon: '/icons/icon-512x512.png',
-  //   vibrate: [100, 50, 100],
-  //   data: {
-  //     dateOfArrival: Date.now()
-  //   },
-  //   actions: [
-  //     {
-  //       action: 'confirm',
-  //       title: '확인'
-  //     },
-  //     {
-  //       action: 'close',
-  //       title: '닫기'
-  //     },
-  //   ],
-  // };
-  // event.waitUntil(self.registration.showNotification(title, options));
-
   const { title, body } = event.data.json();
-  event.waitUntil(self.registration.showNotification(title, {body}));
+  const options = {
+    body: body,
+    icon: '/icons/icon-512x512.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now()
+    },
+    actions: [
+      {
+        action: 'confirm',
+        title: '확인'
+      },
+      {
+        action: 'close',
+        title: '닫기'
+      },
+    ],
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
 };
 
 self.addEventListener('push', pushHandler);
