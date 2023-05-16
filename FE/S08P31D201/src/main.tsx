7b206de8c0e32325292324fb4b79b7259ed3f16b
YongHoon <fishlittle2003@gmail.com>
2023-05-16 23:29:31 +0900

    [FEAT] 메타 태그 추가

--- FE/S08P31D201/src/main.tsx
+++ FE/S08P31D201/src/main.tsx
@@ -5,20 +5,6 @@ import GlobalStyles from '@/styles/GlobalStyles';
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter } from 'react-router-dom';
 
-// const registServiceWorker = async () => {
-//   if ('serviceWorker' in navigator) {
-//     try {
-//       console.log("서비스 워커 등록 성공!");
-      
-//       const worker = await navigator.serviceWorker.register('/service-worker.js');
-//     }
-//     catch (error) {
-//       console.error("서비스 워커 등록 에러...", error);
-//     }
-//   }
-// }
-// registServiceWorker();
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <GlobalStyles />
