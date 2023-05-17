e02ca2278f9fcb874a7b336fb4d14aa57cceac40
sohyeonyun <wja06018@naver.com>
2023-05-17 14:51:31 +0900

    [CHORE] 사원 카드 css

---
+++ FE/S08P31D201/src/components/history/Issue/MemberCardInfo.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { css } from '@emotion/react';
+import { KeyboardArrowDown } from '@mui/icons-material';
+import { ReportUserType, TeamType } from 'ReportTypes';
+
+type MemberCardInfoType = {
+  member: ReportUserType;
+  teamList: TeamType;
+  isTop: boolean;
+};
+
+function MemberCardInfo({ member, teamList, isTop }: MemberCardInfoType) {
+  return (
+    <>
+      <img css={profileImageStyle} src={member.image} alt="" />
+      <div>
+        <p>{member.name}</p>
+        <p>{teamList.teamName}팀</p>
+      </div>
+      <span>{isTop && <KeyboardArrowDown />}</span>
+    </>
+  );
+}
+
+export default MemberCardInfo;
+
+const profileImageStyle = css`
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  border-radius: 50%;
+  box-shadow: 0px 0px 30px 0px rgba(6, 6, 6, 0.15);
+`;
